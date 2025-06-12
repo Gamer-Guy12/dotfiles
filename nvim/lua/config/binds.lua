852cf966e9a97c50646f10f28d74ded3c4a33da1
Varunan Sanjeepan <va.sanjeepan@gmail.com>
2025-06-12 23:52:04 +0000

    Had to add and change some keybinds

--- nvim/lua/config/binds.lua
+++ nvim/lua/config/binds.lua
@@ -6,12 +6,13 @@ vim.keymap.set('n', "<C-k>", "<C-w>k", { silent = true })
 
 -- For tab navigation
 vim.keymap.set('n', "<leader><tab>", ":bnext<CR>", { silent = true })
-vim.keymap.set('n', "<leader><tab>", ":bprev<CR>", { silent = true })
+vim.keymap.set('n', "<leader><S-tab>", ":bprev<CR>", { silent = true })
 
 -- CMD
 
 -- nvim
 vim.keymap.set('n', "<leader>q", ":q<CR>", { silent = true })
+vim.keymap.set('n', "<C-s>", ":w<CR>", { silent = true })
 
 -- LSP
 vim.keymap.set('n', "<leader>a", ":lua vim.lsp.buf.code_action()<CR>", { silent = true })
